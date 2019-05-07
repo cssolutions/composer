1f9c6558d2b87e79f3b411f92c0a8bc89f3247c7
Farkas Csaba <csaba.farkas@digi.co.hu>
2019-05-07 11:49:37 +0200

    modification

---
+++ config/digi-query-logger.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: csaba
+ * Date: 2019.05.07.
+ * Time: 11:45
+ */
+
+return [
+
+];
--- src/Teszt.php
+++ src/Teszt.php
@@ -11,5 +11,40 @@ namespace Teszt;
 
 class Teszt
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected $defer = true;
 
+    /**
+     * Bootstrap the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $config     = __DIR__ . '/../config/digi-query-logger.php';
+
+        $this->publishes([
+            $config     => base_path('config/digi-query-logger.php'),
+        ], 'digi-query-logger');
+
+        $this->mergeConfigFrom($config, 'digi-query-logger');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('QueryLogger', function() {
+            $test = new \stdClass();
+            $test->getName = function($name) {
+                echo $name;
+            };
+            return $test;
+        });
+    }
 }
